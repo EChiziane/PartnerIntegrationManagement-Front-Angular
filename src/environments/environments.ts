45c2b1a33f3ef6905ed0dc263b6e8afe64ab08dc
Chiziane <eddybruno43@gmail.com>
2026-02-06 11:41:54 +0200

    feta(): add CRUD components and services for sprint, manager, driver and carload

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  baseURL: 'http://localhost:8080',
+  baseURL5: 'http://localhost:8080',
   baseURL3: 'http://auth-service-java-spring-angular-production.up.railway.app',
   baseURL2: 'https://auth-service-java-spring-angular-1.onrender.com',
-  baseURL1: 'https://carloadsystemmanagement.onrender.com',
+  baseURL: 'https://carloadsystemmanagement.onrender.com',
   baseURL4: 'https://watersytemmanagementjava-production.up.railway.app',
-  baseURL5: 'https://water-sytem-management-java.onrender.com'
+
 };
