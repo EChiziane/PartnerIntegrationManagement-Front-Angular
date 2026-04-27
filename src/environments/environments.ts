78156db62b3e68d72d20d2c3825720d6c97e103d
Ednilson Chiziane <eddybruno43@gmail.com>
2026-04-27 18:58:45 +0200

    feat(013): refactor customer backend to return dto responses feat(014): improve customer frontend search and form flow feat(015): add customer get by id endpoint and clean service contract

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   baseURL1: 'http://localhost:8080',
-  baseURL: 'https://carloadsystemmanagement.onrender.com',
+  baseURL: 'https://carloadsystemmanagement.fly.dev',
+  baseURL2: 'https://carloadsystemmanagement.onrender.com',
 
 };
